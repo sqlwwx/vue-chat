d438da3e8070e424da667b60ede9179efd9e3822
sqlwwx <wwx_2012@hotmail.com>
2016-06-03 21:15:01 +0800

    format

--- src/components/app.vue
+++ src/components/app.vue
@@ -48,45 +48,44 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div class="sidebar">
-            <card :user="user" :search.sync="search"></card>
-            <list :user-list="userList" :session="session" :session-index.sync="sessionIndex" :search="search"></list>
-        </div>
-        <div class="main">
-            <message :session="session" :user="user" :user-list="userList"></message> 
-            <senders :session></senders>
-        </div>
-    </div>
+<div>
+  <div class="sidebar">
+    <card :user="user" :search.sync="search"></card>
+    <list :user-list="userList" :session="session" :session-index.sync="sessionIndex" :search="search"></list>
+  </div>
+  <div class="main">
+    <message :session="session" :user="user" :user-list="userList"></message>
+    <senders :session></senders>
+  </div>
+</div>
 </template>
 
 <style lang="less">
-    #chat {
-        overflow: hidden;
-        border-radius: 3px;
-        
-        .sidebar, .main {
-            height: 100%;   
-        }
-        .sidebar {
-            float: left;
-            width: 200px;
-            color: #f4f4f4;
-            background-color: #2e3238;
-        }
-        .main {
-            position: relative;
-            overflow: hidden;   
-            background-color: #eee;
-        }
-        .m-text {
-            position: absolute;
-            width: 100%;
-            bottom: 0;
-            left: 0;
-        }
-        .m-message {
-            height: ~'calc(100% - 160px)';
-        }
-    }
+#chat {
+  overflow: hidden;
+  border-radius: 3px;
+  .sidebar, .main {
+    height: 100%;
+  }
+  .sidebar {
+    float: left;
+    width: 200px;
+    color: #f4f4f4;
+    background-color: #2e3238;
+  }
+  .main {
+    position: relative;
+    overflow: hidden;
+    background-color: #eee;
+  }
+  .m-text {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;
+  }
+  .m-message {
+    height: ~'calc(100% - 160px)';
+  }
+}
 </style>
--- src/components/senders/text.vue
+++ src/components/senders/text.vue
@@ -22,24 +22,23 @@ export default {
 </script>
 
 <template>
-    <div class="m-text">
-        <textarea placeholder="按 Ctrl + Enter 发送" v-model="text" @keyup="inputing"></textarea>
-    </div>
+<div class="m-text">
+  <textarea placeholder="按 Ctrl + Enter 发送" v-model="text" @keyup="inputing"></textarea>
+</div>
 </template>
 
 <style lang="less">
-    .m-text {
-        height: 160px;
-        border-top: solid 1px #ddd;
-        
-        textarea {
-            padding: 10px;
-            height: 100%;
-            width: 100%;
-            border: none;
-            outline: none;
-            font-family: "Micrsofot Yahei";
-            resize: none;
-        }
-    }
+.m-text {
+  height: 160px;
+  border-top: solid 1px #ddd;
+  textarea {
+    padding: 10px;
+    height: 100%;
+    width: 100%;
+    border: none;
+    outline: none;
+    font-family: "Micrsofot Yahei";
+    resize: none;
+  }
+}
 </style>
